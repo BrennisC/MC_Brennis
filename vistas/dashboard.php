949537a3d2029ebc976d80024ae1723df5767ba3
Brennis Castro <castrocanobrennis@gmail.com>
2024-11-11 06:54:02 -0500

    vistas del app

--- vistas/dashboard.php
+++ vistas/dashboard.php
@@ -23,7 +23,6 @@
 
     <div class="contenido">
         <h1>Bienvenido al Dashboard</h1>
-        <p>Selecciona una opción del menú para comenzar</p>
         <?php
         if (isset($_GET["opcion"])) {
             $opcion = $_GET["opcion"];
