55cbbd9981a45de13600aa291d5a6a76007a5cf8
Brennis Castro <castrocanobrennis@gmail.com>
2024-11-27 20:09:57 -0500

    index modificado

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 // conexion de la base de datos
 require_once $_SERVER["DOCUMENT_ROOT"] . '/etc/config.php';
-header("location:" . get_controller('controladorLogin.php'));
+header("location:" . get_controllers('controladorLogin.php'));
